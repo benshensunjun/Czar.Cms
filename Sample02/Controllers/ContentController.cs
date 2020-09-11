d8ee917301c9d54fd8bb88c2f83c94c1970264c8
sj <365048862@qq.com>
2020-09-11 11:01:07 +0800

    上传

---
+++ Sample02/Controllers/ContentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers {
+    /// <summary>
+    /// Content控制器
+    /// </summary>
+    public class ContentController : Controller {
+        private readonly Content contents;
+
+        public ContentController(IOptions<Content> option) {
+            contents = option.Value;
+        }
+
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        public IActionResult Index() {
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+
+            /*
+            var contents = new List<Content>();
+            for (int i = 1; i < 11; i++) {
+                contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            }
+            return View(new ContentViewModel { Contents = contents });
+            */
+        }
+    }
+}
---
+++ Sample02/Models/ContentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication1.Models {
+    /// <summary>
+    /// Content视图模式
+    /// </summary>
+    public class ContentViewModel {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
